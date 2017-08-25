ce4e0a9c87ac4d9b996fa3ce675d8a384f358c91
li <2226161723@qq.com>
2017-08-25 13:39:33 +0800

    bugfix

--- js/popupDemo.js
+++ js/popupDemo.js
@@ -3,12 +3,12 @@
  */
 
 window.onload = function () {
-    window.onresize = function () {
+    window.onresize = (function () {
         var sHeight = document.body.scrollHeight || document.documentElement.scrollHeight;
         var sWidth = document.body.scrollWidth || document.documentElement.scrollWidth;
-        console.log("窗口高度：" + sHeight + "; 窗口宽度：" + sWidth);
 
-        function Model(bool, boxHeight, boxWidth) {
+
+        function Model(bool, boxHeight, boxWidth,text) {
 
             if (bool == true) {
                 var oMask = document.createElement("div");
@@ -22,8 +22,20 @@ window.onload = function () {
             oLogin.id = 'smallWindow';
             oLogin.style.height = boxHeight + "px";
             oLogin.style.width = boxWidth + "px";
-            oLogin.innerHTML = "<h1>弹出框</h1><span id='close'>x</span><input type='text' placeholder='请输入....'>";
+            oLogin.innerHTML = "<h1>弹出框</h1><strong id='close'>x</strong><input type='text' placeholder='请输入'>";
+            oLogin.querySelector('h1').innerHTML = text;
             document.body.appendChild(oLogin);
+
+            //获取客户输入内容
+            var inp = oLogin.querySelector('input');
+            var val = localStorage.getItem('userInput')
+            if(val){
+                inp.value = val;
+            }
+            inp.onblur = function () {
+                localStorage.setItem('userInput',inp.value);
+            }
+
 
             //点击关闭按钮关闭登陆框
             var oClose = document.getElementById("close");
@@ -32,19 +44,20 @@ window.onload = function () {
                     document.body.removeChild(oMask);
                 }
                 document.body.removeChild(oLogin);
+                inp.value = localStorage.getItem('userInput');
             };
 
 
         }
 
         document.querySelector("#btn1").onclick = function () {
-            Model(true, 200, 400);
+            Model(true, 200, 400, 'hello');
         }
         document.querySelector("#btn2").onclick = function () {
-            Model(false, 200, 400);
+            Model(false, 200, 400,"随意输入标题");
         }
 
-    };
+    })(window);
 
 }
 
