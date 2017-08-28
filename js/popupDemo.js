792d57b9f968542ab6b37d32800194a064855e09
li <2226161723@qq.com>
2017-08-28 15:33:46 +0800

    重新理解需求

--- js/popupDemo.js
+++ js/popupDemo.js
@@ -2,64 +2,70 @@
  * Created by liwenming3 on 2017/8/25.
  */
 
-window.onload = function () {
-    window.onresize = (function () {
-        var sHeight = document.body.scrollHeight || document.documentElement.scrollHeight;
-        var sWidth = document.body.scrollWidth || document.documentElement.scrollWidth;
+(function(window,undefined){
+    var popup = function(){};
+    popup.prototype= {
+        open: function (options) {
+            this.opt = options;
+            this.popHtml();
+            this.popStyle();
+            this.closeCallBack();
+            this.customDefine();
+        },
+        popHtml:function(){
+            var str="";
 
-
-        function Model(bool, boxHeight, boxWidth,text) {
-
-            if (bool == true) {
-                var oMask = document.createElement("div");
-                oMask.id = "mask";
-                oMask.style.height = sHeight + "px";
-                oMask.style.width = sWidth + "px";
-                document.body.appendChild(oMask);
-
+            str += "<div id='box'><h1>"+this.opt.title+"</h1><strong id='close'>x</strong><div id='contain'></div></div>"
+            if(this.opt.model == true){
+                str += ' <div id="mask"></div> '
             }
-            var oLogin = document.createElement("div");
-            oLogin.id = 'smallWindow';
-            oLogin.style.height = boxHeight + "px";
-            oLogin.style.width = boxWidth + "px";
-            oLogin.innerHTML = "<h1>弹出框</h1><strong id='close'>x</strong><input type='text' placeholder='请输入'>";
-            oLogin.querySelector('h1').innerHTML = text;
-            document.body.appendChild(oLogin);
+            document.body.innerHTML=str;
+        },
 
-            //获取客户输入内容
-            var inp = oLogin.querySelector('input');
-            var val = localStorage.getItem('userInput')
-            if(val){
-                inp.value = val;
+        popStyle:function(){
+            var popUp=document.getElementById("box");
+            var w=(this.opt.width !="" || this.opt.width != undefined) ? this.opt.width+"px":400+"px";
+            var h=(this.opt.height !="" || this.opt.height != undefined) ? this.opt.height+"px":200+"px";
+            popUp.style.cssText = "width:"+w+";height:"+h+";";
+        },
+        closeCallBack:function(){
+            var closeBtn=document.getElementById("close");
+            closeBtn.addEventListener("click", this.closeDiv);
+
+        },
+        //删除弹出层和遮罩层
+        closeDiv:function(){
+
+            var box=document.getElementById("box");
+            var mask=document.getElementById("mask");
+            if(mask){
+                mask.style.display = "none";
             }
-            inp.onblur = function () {
-                localStorage.setItem('userInput',inp.value);
+            box.style.display = "none";
+
+        },
+        customDefine:function ( ){
+            if(this.opt.content){
+                document.getElementById("contain").innerHTML = (this.opt.content)
             }
-
-
-            //点击关闭按钮关闭登陆框
-            var oClose = document.getElementById("close");
-            oClose.onclick = function () {
-                if(bool == true){
-                    document.body.removeChild(oMask);
-                }
-                document.body.removeChild(oLogin);
-                inp.value = localStorage.getItem('userInput');
-            };
-
-
         }
+    }
 
-        document.querySelector("#btn1").onclick = function () {
-            Model(true, 200, 400, 'hello');
-        }
-        document.querySelector("#btn2").onclick = function () {
-            Model(false, 200, 400,"随意输入标题");
-        }
+    window.popup=popup;
 
-    })(window);
+})(window,undefined)
 
-}
+var popup = new popup();
+
+
+
+
+
+
+
+
+
+
 
 
 
