bcaf3be4efbf52fc1ac6671e1ceb7f8ed96c35f7
li <2226161723@qq.com>
2017-08-30 14:08:35 +0800

    options限制

--- js/popupDemo.js
+++ js/popupDemo.js
@@ -2,11 +2,13 @@
  * Created by liwenming3 on 2017/8/25.
  */
 
-(function(window,undefined){
+(function(window){
     var popup = function(){};
     popup.prototype= {
         open: function (options) {
             this.opt = options;
+            this.opt.title = (options.title == "" || options.title == undefined )?"默认title": options.title;
+            this.opt.width = (options.width < 400)? 400:options.width;
             this.popHtml();
             this.popStyle();
             this.closeCallBack();
@@ -15,11 +17,10 @@
         popHtml:function(){
             var str="";
 
-            str += "<div id='box'><h1>"+this.opt.title+"</h1><strong id='close'>x</strong><div id='contain'></div></div>"
+            str += "<div class='box'><h1>"+this.opt.title+"</h1><strong class='close'>x</strong><div class='contain'></div></div>"
             if(this.opt.model == true){
-                str += ' <div id="mask"></div> '
+                str += ' <div class="mask"></div> '
             }
-
             var divObj=document.createElement("div");
             divObj.innerHTML = str;
             var first=document.body.firstChild;//得到页面的第一个元素
@@ -27,21 +28,21 @@
         },
 
         popStyle:function(){
-            var popUp=document.getElementById("box");
-            var w=(this.opt.width !="" || this.opt.width != undefined) ? this.opt.width+"px":400+"px";
-            var h=(this.opt.height !="" || this.opt.height != undefined) ? this.opt.height+"px":200+"px";
+            var popUp=document.getElementsByClassName("box")[0];
+            var w=(this.opt.width !="" && this.opt.width != undefined) ? this.opt.width+"px":400+"px";
+            var h=(this.opt.height !="" && this.opt.height != undefined) ? this.opt.height+"px":200+"px";
             popUp.style.cssText = "width:"+w+";height:"+h+";";
         },
         closeCallBack:function(){
-            var closeBtn=document.getElementById("close");
+            var closeBtn=document.getElementsByClassName("close")[0];
             closeBtn.addEventListener("click", this.closeDiv);
 
         },
         //删除弹出层和遮罩层
         closeDiv:function(){
 
-            var box=document.getElementById("box");
-            var mask=document.getElementById("mask");
+            var box=document.getElementsByClassName("box")[0];
+            var mask=document.getElementsByClassName("mask")[0];
             if(mask){
                 mask.style.display = "none";
             }
@@ -50,14 +51,14 @@
         },
         customDefine:function ( ){
             if(this.opt.content){
-                document.getElementById("contain").innerHTML = (this.opt.content)
+                document.getElementsByClassName("contain")[0].innerHTML = (this.opt.content)
             }
         }
     }
 
     window.popup=popup;
 
-})(window,undefined)
+})(window)
 
 var popup = new popup();
 
